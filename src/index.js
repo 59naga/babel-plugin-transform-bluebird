9b3b6c9a4b45ff81bd2cc2ec50ba102b6e323ec1
59naga <i59naga@icloud.com>
2016-05-15 09:24:06 +0900

    fix: use  instead of

--- src/index.js
+++ src/index.js
@@ -7,53 +7,60 @@ const methods = [
   'race',
 ];
 
-export default {
-  visitor: {
-    Program: {
-      enter(path, file) {
-        file.__UNUSE_BLUEBIRD__ = false;
-        for (const node of path.node.body) {
-          // found "import Promise"
-          const specifiers = node.specifiers || [];
-          const localNames = specifiers.map((specifier) => specifier.local.name);
-          if (localNames.indexOf(defineName) > -1) {
-            file.__UNUSE_BLUEBIRD__ = true;
-            return;
-          }
+export default (babel) => {
+  return ({
+    visitor: {
+      Program: {
+        enter(path, file) {
+          file.__UNUSE_BLUEBIRD__ = false;
+          for (const node of path.node.body) {
+            // found "import Promise"
+            const specifiers = node.specifiers || [];
+            const localNames = specifiers.map((specifier) => specifier.local.name);
+            if (localNames.indexOf(defineName) > -1) {
+              file.__UNUSE_BLUEBIRD__ = true;
+              return;
+            }
 
-          // found "var Promise"
-          const declarations = node.declarations || [];
-          const declarationIds = declarations.map((declaration) => declaration.id.name);
-          if (declarationIds.indexOf(defineName) > -1) {
-            file.__UNUSE_BLUEBIRD__ = true;
-            return;
+            // found "var Promise"
+            const declarations = node.declarations || [];
+            const declarationIds = declarations.map((declaration) => declaration.id.name);
+            if (declarationIds.indexOf(defineName) > -1) {
+              file.__UNUSE_BLUEBIRD__ = true;
+              return;
+            }
           }
+        },
+      },
+
+      // found "new Promise"
+      NewExpression(path, file) {
+        if (file.__UNUSE_BLUEBIRD__) {
+          return;
+        }
+
+        if (path.get('callee').node.name === defineName) {
+          path.node.callee = file.addImport('bluebird', 'default');
         }
       },
-    },
 
-    // found "new Promise"
-    NewExpression(path, file) {
-      if (file.__UNUSE_BLUEBIRD__) {
-        return;
-      }
-
-      if (path.get('callee').node.name === defineName) {
-        path.node.callee = file.addImport('bluebird', 'default');
-      }
-    },
-
-    // found "Promise.methods()"
-    CallExpression(path, file) {
-      if (file.__UNUSE_BLUEBIRD__) {
-        return;
-      }
-
-      methods.forEach((method) => {
-        if (path.get('callee').matchesPattern(`${defineName}.${method}`)) {
-          path.node.callee = file.addImport('bluebird', method);
+      // found "Promise.methods()"
+      CallExpression(path, file) {
+        if (file.__UNUSE_BLUEBIRD__) {
+          return;
         }
-      });
+
+        methods.forEach((method) => {
+          if (path.get('callee').matchesPattern(`${defineName}.${method}`)) {
+            const bluebird = file.addImport('bluebird', 'default');
+            const {
+              memberExpression,
+              identifier,
+            } = babel.types;
+            path.node.callee = memberExpression(bluebird, identifier(method));
+          }
+        });
+      },
     },
-  },
+  });
 };
